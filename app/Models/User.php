448aecf3aef3026029cdc05705988fdcadb82748
MuHajir <hajir.nf@gmail.com>
2021-09-07 20:10:25 +0700

    implement laravel/passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,25 @@
 
 namespace App\Models;
 
+
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable
 {
-    
+    use HasFactory, Notifiable, HasApiTokens, SoftDeletes;
+
+    protected $fillable = [
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
 }
