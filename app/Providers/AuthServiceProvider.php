45fd18eb551c2382487d50559f1d1bc985937d3e
MuHajir <hajir.nf@gmail.com>
2021-09-08 14:39:28 +0700

    create gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('view-all-chat', function (User $user) {
+            return $user->user_type_id == User::STAFF;
+        });
+        Gate::define('view-all-customer', function (User $user) {
+            return $user->user_type_id == User::STAFF;
+        });
+        Gate::define('delete-customer', function (User $user) {
+            return $user->user_type_id == User::STAFF;
+        });
     }
 }
